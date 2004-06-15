aae722fc190ea973d6178b7d8b188237af5f8d06
Peter Andrews <petea@jhu.edu>
2004-06-15 02:26:57 +0000

    added french locale

--- src/install.js
+++ src/install.js
@@ -6,6 +6,7 @@ const APP_CONTENTS_PATH		= "content/";
 const APP_SKIN_CLASSIC_PATH	= "skin/classic/";
 const APP_LOCALE_ENUS_PATH	= "locale/en-US/";
 const APP_LOCALE_JAJP_PATH	= "locale/ja-JP/";
+const APP_LOCALE_FRFR_PATH	= "locale/fr-FR/";
 
 
 initInstall(APP_NAME, APP_CHROME_NAME, APP_VERSION); 
@@ -19,6 +20,7 @@ registerChrome(CONTENT | PROFILE_CHROME, jarFolder, APP_CONTENTS_PATH);
 registerChrome(SKIN | PROFILE_CHROME, jarFolder, APP_SKIN_CLASSIC_PATH);
 registerChrome(LOCALE | PROFILE_CHROME, jarFolder, APP_LOCALE_ENUS_PATH);
 registerChrome(LOCALE | PROFILE_CHROME, jarFolder, APP_LOCALE_JAJP_PATH);
+registerChrome(LOCALE | PROFILE_CHROME, jarFolder, APP_LOCALE_FRFR_PATH);
 
 var result = getLastError(); 
 if(result == SUCCESS) {
