e664a1c160d93aedcc9a85189b85fa03886d65f1
Peter Andrews <petea@jhu.edu>
2005-01-19 05:04:13 +0000

    license headers and misc cleanup

--- src/sage/content/search/feedsearch.js
+++ src/sage/content/search/feedsearch.js
@@ -1,9 +1,46 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Sage.
+ *
+ * The Initial Developer of the Original Code is
+ * Peter Andrews <petea@jhu.edu>.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Peter Andrews <petea@jhu.edu>
+ * Erik Arvidsson <erik@eae.net>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 var FeedSearch = {
 	RRP_NS: "http://sage.mozdev.org/#",
 	SEARCH_ENGINE_RDF: "chrome://sage/content/search/searchEngine.rdf",
 
-	RDF:Components.classes['@mozilla.org/rdf/rdf-service;1']
-			.getService(Components.interfaces.nsIRDFService),
+	RDF: Components.classes['@mozilla.org/rdf/rdf-service;1'].getService(Components.interfaces.nsIRDFService),
 
 	txtSearchValue: null,
 	imgSearchEngine: null,
@@ -20,18 +57,18 @@ var FeedSearch = {
 		this.imgSearchEngine = document.getElementById("imgSearchEngine");		
 		this.popSearchEngine = document.getElementById("popSearchEngine");
 		
-			// init Search Engine RDF DataSource
+		// init Search Engine RDF DataSource
 		this.rdfDS = this.RDF.GetDataSource(this.SEARCH_ENGINE_RDF);
 		var remote = this.rdfDS.QueryInterface(Components.interfaces.nsIRDFRemoteDataSource);
-		if(remote.loaded){
+		if(remote.loaded) {
 			this.initRdfDataSource();
-		}else{
+		} else {
 			var sink = this.rdfDS.QueryInterface(Components.interfaces.nsIRDFXMLSink);
 			sink.addXMLSinkObserver(this.sinkObserver);
 		}
 	},
 	
-	initRdfDataSource: function(){
+	initRdfDataSource: function() {
 		this.popSearchEngine.database.AddDataSource(this.rdfDS);
 		this.popSearchEngine.builder.rebuild();
 		var tmpSearchEngine = this.txtSearchValue.getAttribute("searchengine");
@@ -40,20 +77,16 @@ var FeedSearch = {
 		this.setSearchEngine(tmpSearchEngine);	
 	},
 	
-	search: function(){
+	search: function() {
 		var searchValue = this.txtSearchValue.value;
 		if(searchValue == "") return;
 
 		var searchName = this.searchEngineName + " - " + searchValue;
 
-			// 検索履歴に検索文字列を追加
-		var formHistory = Components.classes["@mozilla.org/satchel/form-history;1"]
-							.getService(Components.interfaces.nsIFormHistory);
+		var formHistory = Components.classes["@mozilla.org/satchel/form-history;1"].getService(Components.interfaces.nsIFormHistory);
 		formHistory.addEntry("q", searchValue);
 
-			// 検索文字列を検索エンジンの文字コードに合わせてエスケープ
-		var textToSubURI = Components.classes["@mozilla.org/intl/texttosuburi;1"]
-								.getService(Components.interfaces.nsITextToSubURI);
+		var textToSubURI = Components.classes["@mozilla.org/intl/texttosuburi;1"].getService(Components.interfaces.nsITextToSubURI);
 		searchValue = textToSubURI.ConvertAndEscape(this.charset, searchValue);
 
 		lastResource = {
@@ -66,7 +99,7 @@ var FeedSearch = {
 		httpGet(lastResource.url);
 	},
 	
-	setSearchEngine: function(aSearchEngine){
+	setSearchEngine: function(aSearchEngine) {
 		this.searchEngine = aSearchEngine;
 		this.searchEngineName = this.getRdfProperty(aSearchEngine, this.RRP_NS + "name");
 		this.query = this.getRdfProperty(aSearchEngine, this.RRP_NS + "query");
@@ -76,29 +109,29 @@ var FeedSearch = {
 		this.imgSearchEngine.src = this.getRdfProperty(aSearchEngine, this.RRP_NS + "icon");
 	},
 	
-	popSearchEngineClick: function(aEvent){
+	popSearchEngineClick: function(aEvent) {
 		var menuitemNode = aEvent.originalTarget;
 		if(menuitemNode.nodeName != "menuitem") return;
 		
 		this.setSearchEngine(menuitemNode.value);
 	},
 	
-	getRdfProperty: function(aRes, aProperty){
+	getRdfProperty: function(aRes, aProperty) {
 		if(typeof(aRes) == "string") aRes = this.RDF.GetResource(aRes);
 		if(typeof(aProperty) == "string") aProperty = this.RDF.GetResource(aProperty);
 		var target = this.rdfDS.GetTarget(aRes, aProperty, true);
-		try{
+		try {
 			return target.QueryInterface(Components.interfaces.nsIRDFLiteral).Value;
-		}catch(e){
+		} catch(e) {
 			return target.QueryInterface(Components.interfaces.nsIRDFResource).Value;
 		}
 	},
 	
 	sinkObserver: {
-		onBeginLoad: function(aSink){},
-		onInterrupt: function(aSink){},
-		onResume: function(aSink){},
-		onEndLoad: function(aSink){ FeedSearch.initRdfDataSource() },
-		onError: function(aSink, aStatus, aErrorMsg){}
+		onBeginLoad: function(aSink) {},
+		onInterrupt: function(aSink) {},
+		onResume: function(aSink) {},
+		onEndLoad: function(aSink) { FeedSearch.initRdfDataSource() },
+		onError: function(aSink, aStatus, aErrorMsg) {}
 	}
 }
--- src/sage/content/settings/bookmarkfoldermenulist.js
+++ src/sage/content/settings/bookmarkfoldermenulist.js
@@ -1,39 +1,76 @@
-function BookmarkFolderMenuList(aMenulistID, aRootFolderID){
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Sage.
+ *
+ * The Initial Developer of the Original Code is
+ * Peter Andrews <petea@jhu.edu>.
+ * Portions created by the Initial Developer are Copyright (C) 2005
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Peter Andrews <petea@jhu.edu>
+ * Erik Arvidsson <erik@eae.net>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+function BookmarkFolderMenuList(aMenulistID, aRootFolderID) {
 	this.menuList = document.getElementById(aMenulistID);
 	this.menupopup = this.menuList.menupopup;
 	this.rootFolder = aRootFolderID ? aRootFolderID : "NC:BookmarksRoot";
 	this.init();
 }
+
 BookmarkFolderMenuList.prototype = {
-	RDF: Components.classes["@mozilla.org/rdf/rdf-service;1"]
-				.getService(Components.interfaces.nsIRDFService),
-	RDFC: Components.classes["@mozilla.org/rdf/container;1"]
-				.getService(Components.interfaces.nsIRDFContainer),
-	RDFCU: Components.classes["@mozilla.org/rdf/container-utils;1"]
-				.getService(Components.interfaces.nsIRDFContainerUtils),
+
+	RDF: Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService),
+	RDFC: Components.classes["@mozilla.org/rdf/container;1"].getService(Components.interfaces.nsIRDFContainer),
+	RDFCU: Components.classes["@mozilla.org/rdf/container-utils;1"].getService(Components.interfaces.nsIRDFContainerUtils),
 	BMDS: null,
 
 
-	set rootFolderID(aValue){
+	set rootFolderID(aValue) {
 		this.rootFolder = aValue;
 		this.init();
 		return this.rootFolder;
 	},
-	get rootFolderID(){
+	get rootFolderID() {
 		return this.rootFolder;
 	},
 
-	set currentFolderID(aValue){
+	set currentFolderID(aValue) {
 		return this.menuList.value = aValue;
 	},
-	get currentFolderID(){
+	get currentFolderID() {
 		return this.menuList.value;
 	},
 
-	init: function(){
+	init: function() {
 		this.BMDS = this.RDF.GetDataSource("rdf:bookmarks");
 		
-		while (this.menupopup.hasChildNodes()){
+		while (this.menupopup.hasChildNodes()) {
 			this.menupopup.removeChild(this.menupopup.firstChild);
 		}
 
@@ -46,10 +83,10 @@ BookmarkFolderMenuList.prototype = {
 		this.menuList.selectedIndex = 0;
 	},
 
-	createMenuItem: function(aFolder, aDepth){
+	createMenuItem: function(aFolder, aDepth) {
 		this.RDFC.Init(this.BMDS, aFolder);
 		var children = this.RDFC.GetElements();
-		while (children.hasMoreElements()){
+		while (children.hasMoreElements()) {
 			var item = children.getNext();
 			if(!this.RDFCU.IsContainer(this.BMDS, item)) continue;
 			
