07a7cc3063f2fb1b2ade4a7331f007f46f70addf
Erik Arvidsson <erik.arvidsson@gmail.com>
2005-02-03 19:57:06 +0000

    Merging createhtml updates from FeedSummary_20050131 branch

--- src/sage/content/createhtml.js
+++ src/sage/content/createhtml.js
@@ -43,14 +43,16 @@ var CreateHTML = {
 
 	_tabbed: false,
 
-	set tabbed(aValue){ this._tabbed = aValue },
+	set tabbed(aValue) { this._tabbed = aValue },
 
 	openHTML: function(feed) {
-		if(!feed) return;
+		if (!feed) {
+			return;
+		}
 
 		try {
 			var htmlURL = this.createHTML(feed);
-			if(this._tabbed) {
+			if (this._tabbed) {
 				getContentBrowser().addTab(htmlURL);
 			} else {
 				getContentBrowser().loadURI(htmlURL);
@@ -68,7 +70,7 @@ var CreateHTML = {
 							.getService(Components.interfaces.nsIIOService);
 		var xmlFilePath = ioService.newFileURI(tmpFile).spec;
 
-		if(tmpFile.exists()) {
+		if (tmpFile.exists()) {
 			tmpFile.remove(true);
 		}
 		tmpFile.create(tmpFile.NORMAL_FILE_TYPE, 0666);
@@ -88,7 +90,15 @@ var CreateHTML = {
 	getUserCssURL: function() {
 		var userCssEnable = CommonFunc.getPrefValue(CommonFunc.USER_CSS_ENABLE, "bool", false);
 		var userCssPath = CommonFunc.getPrefValue(CommonFunc.USER_CSS_PATH, "wstr", "");
-		if(!userCssEnable || !userCssPath) return null;
+		if (!userCssEnable || !userCssPath) {
+			return null;
+		}
+
+		return userCssPath;
+
+		/*
+
+        we need to check for file path and convert to uri and check uris
 
 		var ioService = Components.classes["@mozilla.org/network/io-service;1"]
 							.getService(Components.interfaces.nsIIOService);
@@ -98,108 +108,184 @@ var CreateHTML = {
 			tmpFile.initWithPath(userCssPath);
 			var cssUrl = ioService.newFileURI(tmpFile);
 			var contentType = ioService.newChannelFromURI(cssUrl).contentType;
-			if(contentType != "text/css") return null;
+			if (contentType != "text/css") {
+				return null;
+            }
 
 			return cssUrl.spec;
 		} catch(e) {
 			return null;
 		}
+		*/
+	},
+
+	formatFileSize:	function (n) {
+		if (n > 1048576) {
+			return Math.round(n / 1048576) + "M";
+		} else if (n > 1024) {
+			return Math.round(n / 1024) + "K";
+		} else {
+			return n + "B";
+		}
 	},
 
 	createHTMLSource: function(feed) {
-		var allowEContent = CommonFunc.getPrefValue(CommonFunc.ALLOW_ENCODED_CONTENT, "bool", true);
-		var twelveHourClock = CommonFunc.getPrefValue(CommonFunc.TWELVE_HOUR_CLOCK, "bool", false);
+		return this.HTML_SOURCE.replace(/\*\*[^\*]+\*\*/g, function (s) {
+				return CreateHTML.replaceFeedKeyword(feed, s);
+			});
+
+		return CommonFunc.convertCharCodeFrom(
+			this.HTML_SOURCE.replace(/\*\*[^\*]+\*\*/g, function (s) {
+				return CreateHTML.replaceFeedKeyword(feed, s);
+			}),
+			"UTF-8");
+	},
+
+	replaceFeedKeyword:	function (feed, s) {
+		var footer;
+
+		switch (s) {
+			case "**CSSURL**":
+				return this.getUserCssURL() || this.DEFAULT_CSS;
+
+			case "**HTMLTITLE**":
+			case "**TITLE**":
+				return feed.getTitle();
+
+			case "**LINK**":
+				return feed.getLink();
+				break;
+
+			case "**AUTHOR**":
+				return feed.getAuthor();
+
+			case "**DESCRIPTION**":
+				return feed.getDescription();
+
+			case "**LOGOLINK**":
+				return feed.getLogo().link;
+
+			case "**LOGOALT**":
+				return feed.getLogo().alt;
+
+			case "**COPYRIGHT**":
+				return feed.getFooter().copyright;
+
+			case "**GENERATOR**":
+				return feed.getFooter().generator;
+
+			case "**EDITOR**":
+				var editor = "";
+				footer = feed.getFooter();
+				if (footer.editor) {
+					editor = "<a href=\"mailto:" + footer.editor + "\">Editor</a>";
+					if (footer.webmaster) {
+						editor += ", ";
+					}
+				}
+				return editor;
+
+			case "**WEBMASTER**":
+				footer = feed.getFooter();
+				if (footer.webmaster) {
+					return "<a href=\"mailto:" + footer.webmaster + "\">" +
+						strRes.getString("feed_summary_webmaster") +
+						"</a>";
+				}
+				return "";
+
+			case "**ITEMS**":
+				return this.getItemsHtml(feed);
+		}
+
+		return s;
+	},
+
+	getItemsHtml:	function (feed) {
 		var feedItemOrder = CommonFunc.getPrefValue(CommonFunc.FEED_ITEM_ORDER, "str", "chrono");
-
-		var htmlSource = this.HTML_SOURCE;
-		var cssUrl	= this.getUserCssURL();
-		if(cssUrl) {
-			htmlSource = htmlSource.replace("**CSSURL**", cssUrl);
-		} else {
-			htmlSource = htmlSource.replace("**CSSURL**", this.DEFAULT_CSS);
-		}
-		htmlSource = htmlSource.replace("**HTMLTITLE**", feed.getTitle());
-		htmlSource = htmlSource.replace("**TITLE**", feed.getTitle());
-		htmlSource = htmlSource.replace(/\*\*LINK\*\*/g, feed.getLink());
-		htmlSource = htmlSource.replace("**AUTHOR**", feed.getAuthor());
-		htmlSource = htmlSource.replace("**DESCRIPTION**", feed.getDescription());
-		htmlSource = htmlSource.replace("**LOGOLINK**", feed.getLogo().link);
-		htmlSource = htmlSource.replace("**LOGOALT**", feed.getLogo().alt);
-
-		var footer = feed.getFooter();
-		htmlSource = htmlSource.replace("**COPYRIGHT**", footer.copyright);
-		htmlSource = htmlSource.replace("**GENERATOR**", footer.generator);
-		var editor = "";
-		var webmaster = "";
-		if (footer.editor) {
-			editor = "<a href=\"mailto:" + footer.editor + "\">Editor</a>";
-			if (footer.webmaster)
-				editor += ", ";
-		}
-		htmlSource = htmlSource.replace("**EDITOR**", editor);
-
-		// TODO: localizations
-		if (footer.webmaster) {
-			webmaster = "<a href=\"mailto:" + footer.webmaster + "\">Webmaster</a>";
-		}
-		htmlSource = htmlSource.replace("**WEBMASTER**", webmaster);
-
-		var itemsSource = "";
 		var items = feed.getItems(feedItemOrder);
-		for(var i = 0; i < items.length; i++) {
-			var link = items[i].getLink();
-			var title = items[i].getTitle();
-			var author = items[i].getAuthor();
-			var enclosure = "";
-			var description = "";
-			var pubDate = "";
-
-			if(items[i].hasContent()) {
-				description = allowEContent ? items[i].getContent() : htmlToText(items[i].getContent());
-				description = "<div class=\"item-desc\">" + description + "</div>";
-			}
-
-			// TODO: localizations
-			if(items[i].hasAuthor()) {
-				author = "<div class=\"item-author\">By " + author + "</div>";
-			}
-
-			if(items[i].hasPubDate()) {
-				pubDate = "<div class=\"item-pubDate\">" + dateFormat(items[i].getPubDate(), twelveHourClock) + "</div>";
-			}
-
-			if(items[i].hasEnclosure()) {
-				var tmp = items[i].getEnclosure();
-				enclosure = "<div class=\"item-enclosure\">" +
-					// TODO: localizations
-					"<a href=\"" + tmp.getLink() + "\" title=\"Enclosure\">" +
-					"<img src=\"" +
-					(tmp.hasMimeType() ?
-						"moz-icon://goat?size=16&contentType=" + tmp.getMimeType() :
-						"chrome://sage/skin/enclosure.png") +
-					"\"></a> " +
-					(tmp.getDesc() ? tmp.getDesc() + ", " : "") +
-					tmp.getSize() + "</div>";
-			}
-
-			var itemSource = this.ITEM_SOURCE;
-			itemSource = itemSource.replace("**NUMBER**", i+1);
-			itemSource = itemSource.replace("**LINK**", link);
-			itemSource = itemSource.replace("**TECHNORATI**", encodeURIComponent(link));
-			itemSource = itemSource.replace("**TITLE**", title);
-			itemSource = itemSource.replace("**AUTHOR**", author);
-			itemSource = itemSource.replace("**DESCRIPTION**", description);
-			itemSource = itemSource.replace("**PUBDATE**", pubDate);
-			itemSource = itemSource.replace("**ENCLOSURE**", enclosure);
-			itemsSource += itemSource;
+		var sb = [];
+		for (var i = 0; i < items.length; i++) {
+			sb.push(this.getItemHtml(feed, items[i], i));
 		}
-		htmlSource = htmlSource.replace("**ITEMS**", itemsSource);
+		return sb.join("");
+	},
 
-		return CommonFunc.convertCharCodeFrom(htmlSource, "UTF-8");
+	getItemHtml:	function (feed, item, i) {
+		return  this.ITEM_SOURCE.replace(/\*\*[^\*]+\*\*/g, function (s) {
+			return CreateHTML.replaceFeedItemKeyword(feed, item, i, s);
+		});
+	},
+
+	replaceFeedItemKeyword:	function (feed, item, i, s) {
+		switch (s) {
+			case "**NUMBER**":
+				return i  +1;
+
+			case "**LINK**":
+				return item.getLink();
+
+			case "**TECHNORATI**":
+				return encodeURIComponent(item.getLink());
+
+			case "**TITLE**":
+				return item.getTitle();
+
+			case "**AUTHOR**":
+				return item.getAuthor();
+
+			case "**DESCRIPTION**":
+				if (item.hasContent()) {
+					var allowEContent = CommonFunc.getPrefValue(CommonFunc.ALLOW_ENCODED_CONTENT, "bool", true);
+					var ds;
+					if (allowEContent) {
+						this.filterHtmlHandler.clear();
+						this.simpleHtmlParser.parse(item.getContent());
+						ds = this.filterHtmlHandler.toString();
+					} else {
+						ds = htmlToText(item.getContent());
+					}
+					return "<div class=\"item-desc\">" + ds + "</div>";
+				}
+				return "";
+
+			case "**PUBDATE**":
+				if (item.hasPubDate()) {
+					var twelveHourClock = CommonFunc.getPrefValue(CommonFunc.TWELVE_HOUR_CLOCK, "bool", false);
+					return "<div class=\"item-pubDate\">" +
+						dateFormat(item.getPubDate(), twelveHourClock) +
+						"</div>";
+				}
+				return "";
+
+			case "**ENCLOSURE**":
+				if (item.hasEnclosure()) {
+					var enc = item.getEnclosure();
+					return "<div class=\"item-enclosure\">" +
+						"<a href=\"" + enc.getLink() + "\" title=\"" +
+						strRes.getString("feed_summary_enclosure") +
+						"\"><img src=\"" +
+							(enc.hasMimeType() ?
+								"moz-icon://dummy?size=16&contentType=" + enc.getMimeType() :
+								"chrome://sage/skin/enclosure.png") +
+						"\"> " +
+						(enc.getDescription() ? enc.getDescription() + ", " : "") +
+						(enc.hasLength() ? this.formatFileSize(enc.getLength()) : "") +
+						"</a></div>";
+				}
+				return "";
+		}
+
+		return s;
 	},
 
 	getSpecialDir: function(aProp) {
 		var dirService = Components.classes['@mozilla.org/file/directory_service;1'].getService(Components.interfaces.nsIProperties);
 		return dirService.get(aProp, Components.interfaces.nsILocalFile);
-	}
-}
+	},
+
+	simpleHtmlParser:	new SimpleHtmlParser,
+	filterHtmlHandler:	new FilterHtmlHandler
+};
+
+CreateHTML.simpleHtmlParser.contentHandler = CreateHTML.filterHtmlHandler
