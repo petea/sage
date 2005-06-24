88a02ec0d36e17ba1c9a623618636db95e3c5385
Peter Andrews <petea@jhu.edu>
2005-06-24 05:50:00 +0000

    bump version to 1.3.5

--- src/sage/content/commonfunc.js
+++ src/sage/content/commonfunc.js
@@ -69,7 +69,7 @@ function dateFormat(date, twelveHourClock, format) {
 
 var CommonFunc = {
 
-	VERSION: Array(1,3,4),
+	VERSION: Array(1,3,5),
 	USER_AGENT: "Mozilla/5.0 (Sage)",
 
 	FEED_FOLDER_ID: "sage.folder_id",
