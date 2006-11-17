f3edc743d8c70c016051474b3d14b9eb457ce9d1
Peter Andrews <petea@jhu.edu>
2006-11-17 18:03:38 +0000

    [15767] handle tags with multiple opening brackets

--- src/sage/content/simplehtmlparser.js
+++ src/sage/content/simplehtmlparser.js
@@ -53,7 +53,7 @@ SimpleHtmlParser.prototype = {
 
 	// regexps
 
-	startTagRe:	/^<([^>\s\/]+)((\s+[^=>\s]+(\s*=\s*((\"[^"]*\")|(\'[^']*\')|[^>\s]+))?)*)\s*\/?\s*>/m,
+	startTagRe:	/^<([^<>\s\/]+)((\s+[^=<>\s]+(\s*=\s*((\"[^"]*\")|(\'[^']*\')|[^>\s]+))?)*)\s*\/?\s*>/m,
 	endTagRe:	/^<\/([^>\s]+)[^>]*>/m,
 	attrRe:		/([^=\s]+)(\s*=\s*((\"([^"]*)\")|(\'([^']*)\')|[^>\s]+))?/gm,
 
@@ -114,7 +114,7 @@ SimpleHtmlParser.prototype = {
 			if (treatAsChars) {
 				index = s.indexOf("<");
 				if (index == -1) {
-					 this.contentHandler.characters(s);
+					this.contentHandler.characters(s);
 					s = "";
 				} else {
 					if (index == 0) {	// in case we got a < in the character stream
