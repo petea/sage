76b1551152f8582cfb38dc0947c118e7f84e0280
Peter Andrews <petea@jhu.edu>
2010-03-04 02:03:27 -0800

    Insert feed content directly into DOM.  Fixes issue 4.

--- src/sage/content/feedsummary.js
+++ src/sage/content/feedsummary.js
@@ -47,6 +47,8 @@ var strBundleService = Components.classes["@mozilla.org/intl/stringbundle;1"].ge
 var strRes = strBundleService.createBundle("chrome://sage/locale/sage.properties");
 
 var feedSummary = {
+		
+	DEFAULT_CSS: "chrome://sage/skin/feedsummary-content.css",
 
 	_uri:			null,
 	_feedLoader:	null,
@@ -189,10 +191,10 @@ var feedSummary = {
 		// This should be handled in a better way.
 		if (aFeed.getFeedURI() == feedSummary.uri)
 		{
-			feedSummary.displayFeed(aFeed);
-
 			document.body.removeAttribute("loading");
 			document.body.removeAttribute("error");
+			
+			feedSummary.displayFeed(aFeed);
 		}
 		else
 		{
@@ -233,7 +235,131 @@ var feedSummary = {
 	displayFeed:	function (feed)
 	{
 		document.title = feed.getTitle() + " - Sage";
-		document.body.innerHTML = CreateHTML.createHTMLSource(feed);
+		
+		var allowEContent = SageUtils.getSagePrefValue(SageUtils.PREF_ALLOW_ENCODED_CONTENT);
+		var twelveHourClock = SageUtils.getSagePrefValue(SageUtils.PREF_TWELVE_HOUR_CLOCK);
+		var sanitizer = Cc["@mozilla.org/feed-unescapehtml;1"].getService(Ci.nsIScriptableUnescapeHTML);
+		var dateFormatter = Cc["@sage.mozdev.org/sage/dateformatter;1"].getService(Ci.sageIDateFormatter);
+		dateFormatter.setFormat(dateFormatter.FORMAT_LONG, dateFormatter.ABBREVIATED_FALSE, twelveHourClock ? dateFormatter.CLOCK_12HOUR : dateFormatter.CLOCK_24HOUR);
+		var content;
+		
+		var header = document.createElement("div");
+		header.setAttribute("id", "rss-header");
+		var h1 = document.createElement("h1");
+		h1.setAttribute("id", "rss-title");
+		var feedLink = document.createElement("a");
+		feedLink.setAttribute("id", "rss-link");
+		this.setURIAttributeSafe(feedLink, "href", feed.getLink());
+		feedLink.appendChild(document.createTextNode(feed.getTitle()));
+		h1.appendChild(feedLink);
+		header.appendChild(h1);
+		if (feed.hasDescription()) {
+			var p = document.createElement("p");
+			p.setAttribute("id", "rss-desc");
+			if (allowEContent) {
+				content = feed.getDescription();
+			} else {
+				content = SageUtils.htmlToText(feed.getDescription());
+			}
+			p.appendChild(sanitizer.parseFragment(content, false, null, document.documentElement));
+			header.appendChild(p);
+		}
+		document.body.appendChild(header);
+		
+		function renderFeedItem(feedItem, index) {
+			var item = document.createElement("div");
+			item.setAttribute("class", "item");
+			var h2 = document.createElement("h2");
+			h2.setAttribute("class", "item-title");
+			var span = document.createElement("span");
+			span.setAttribute("class", "item-ordinal");
+			span.appendChild(document.createTextNode((index + 1) + ". " ));
+			h2.appendChild(span);
+			var a = document.createElement("a");
+			this.setURIAttributeSafe(a, "href", feedItem.getLink());
+			function getFeedItemTitle(feedItem) {
+				if (feedItem.hasTitle()) {
+					return feedItem.getTitle();
+				} else if (feedItem.getTitle()) {
+					return feedItem.getTitle();
+				} else {
+					return strRes.GetStringFromName("feed_item_no_title");
+				}
+			}
+			a.appendChild(document.createTextNode(getFeedItemTitle(feedItem)));
+			h2.appendChild(a);
+			item.appendChild(h2);
+			if (feedItem.hasContent()) {
+				var description = document.createElement("div");
+				description.setAttribute("class", "item-desc");
+				if (allowEContent) {
+					content = feedItem.getContent();
+				} else {
+					content = SageUtils.htmlToText(feedItem.getContent());
+				}
+				description.appendChild(sanitizer.parseFragment(content, false, null, document.documentElement));
+				item.appendChild(description);
+			}
+			if (feedItem.hasEnclosure()) {
+				var enc = item.getEnclosure();
+				var enclosure = document.createElement("div");
+				enclosure.setAttribute("class", "item-enclosure");
+				a = document.createElement("a");
+				this.setURIAttributeSafe(a, "href", enc.getLink());
+				function createDescriptionFromURL(url) {
+					var array = url.split("/");
+					var description = "";
+					if (array.length > 0) {
+						description = array[array.length - 1];
+					}
+					return description;
+				}
+				a.setAttribute("title", createDescriptionFromURL(enc.getLink()));
+				var img = document.createElement("img");
+				img.setAttribute("src", enc.hasMimeType() ? "moz-icon://dummy?size=16&contentType=" + enc.getMimeType() : "chrome://sage/skin/enclosure.png");
+				a.appendChild(img);
+				a.appendChild(document.createTextNode(strRes.GetStringFromName("feed_summary_enclosure")));
+				enclosure.appendChild(a);
+				function formatFileSize(n) {
+					if (n > 1048576) {
+						return Math.round(n / 1048576) + " MB";
+					} else if (n > 1024) {
+						return Math.round(n / 1024) + " KB";
+					} else {
+						return n + " B";
+					}
+				}
+				enclosure.appendChild(document.createTextNode(enc.hasLength() ? " (" + formatFileSize(enc.getLength()) + ")" : ""));
+				item.appendChild(enclosure);
+			}
+			if (feedItem.hasPubDate()) {
+				var pubdate = document.createElement("div");
+				pubdate.setAttribute("class", "item-pubDate");
+				pubdate.appendChild(document.createTextNode(dateFormatter.formatDate(feedItem.getPubDate())));
+				item.appendChild(pubdate);
+			}
+			if (feedItem.hasAuthor()) {
+				var author = document.createElement("div");
+				author.setAttribute("class", "item-author");
+				author.appendChild(document.createTextNode(feedItem.getAuthor()));
+				item.appendChild(author);
+			}
+			document.body.appendChild(item);
+		}
+		
+		if (feed.getItemCount() > 0) {
+			var items = document.createElement("div");
+			items.setAttribute("class", "items");
+			var feedItemOrder = SageUtils.getSagePrefValue(SageUtils.PREF_FEED_ITEM_ORDER);
+			switch (feedItemOrder) {
+				case "chrono": feed.setSort(feed.SORT_CHRONO); break;
+				case "source": feed.setSort(feed.SORT_SOURCE); break;
+			}
+			for (var i = 0; i < feed.getItemCount(); i++) {
+				renderFeedItem.call(this, feed.getItem(i), i);
+			}
+			document.body.appendChild(items);
+		}
 	},
 
 	findSageSideBar:	function ()
@@ -346,8 +472,45 @@ var feedSummary = {
 		}
 
 		return false;
+	},
+		
+	setURIAttributeSafe: function(element, attribute, uri) {
+		var secman = Cc["@mozilla.org/scriptsecuritymanager;1"].getService(Ci.nsIScriptSecurityManager);
+		var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);		
+		
+		var feedURI = null;
+		try {
+			feedURI = ios.newURI(this.uri, null, null);
+		} catch (e) { }
+		
+		var feedPrincipal = secman.getCodebasePrincipal(feedURI);
+		const flags = Ci.nsIScriptSecurityManager.DISALLOW_INHERIT_PRINCIPAL;
+		try {
+			secman.checkLoadURIStrWithPrincipal(feedPrincipal, uri, flags);
+		} catch (e) {
+			return;
+		}
+		element.setAttribute(attribute, uri);
+	},
+	
+	getUserCssURL: function() {
+		var userCssEnable = SageUtils.getSagePrefValue(SageUtils.PREF_USER_CSS_ENABLE);
+		var userCssPath = SageUtils.getSagePrefValue(SageUtils.PREF_USER_CSS_PATH);
+		if (!userCssEnable || !userCssPath) {
+			return null;
+		}
+		var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+		var tmpFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+		try {
+			tmpFile.initWithPath(userCssPath);
+			var cssUrl = ioService.newFileURI(tmpFile);
+			var contentType = ioService.newChannelFromURI(cssUrl).contentType;
+			return cssUrl.spec;
+		} catch(e) {
+			return null;
+		}
 	}
-
+	
 };
 
 window.addEventListener("load", function (e)
@@ -367,9 +530,9 @@ if (feedSummary.uri) {
 }
 
 // set feed style sheet before content loads
-var cssUrl	= CreateHTML.getUserCssURL();
+var cssUrl	= feedSummary.getUserCssURL();
 if (!cssUrl) {
-	cssUrl = CreateHTML.DEFAULT_CSS;
+	cssUrl = feedSummary.DEFAULT_CSS;
 }
 var headEl = document.getElementsByTagName("head")[0];
 var linkEl = document.createElement("link");
